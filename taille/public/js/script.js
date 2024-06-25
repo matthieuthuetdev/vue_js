dd2b5514ae593f2b3217c8b2b0678df66d9124ba
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-06-25 16:55:49 +0200

    continuer le vuejs

--- taille/public/js/script.js
+++ taille/public/js/script.js
@@ -2,13 +2,26 @@ const app = {
     data() {
         return {
             taille: 16,
+            min: 8,
             max: 48
         }
     },
     methods: {
         agrendire() {
-            this.taille++
+            if (this.taille >= this.max) {
+                this.taille = 16;
+            } else {
+                this.taille++
+            }
+        },
+        retrecir() {
+            if (this.taille <= this.min) {
+                this.taille = 16
+            } else {
+                this.taille--
+            }
         }
+
     }
 }
 Vue.createApp(app).mount("#app")
