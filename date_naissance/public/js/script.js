e858954871e4eeb622ce9df16c7656a3eec086a7
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-06-27 15:47:47 +0200

    exercice date de naissance

---
+++ date_naissance/public/js/script.js
@@ -0,0 +1,12 @@
+const app = {
+    data(){
+        return{
+            date_naissance: "",
+            date_atcuel: Date.toLocaldatString()
+        }
+    },
+    calculer(){
+        tolocaldat
+    }
+}
+Vue.createApp(app).mount("#date")
