6654a1344d937fd5c1cf526c25b3ec1860ef6827
matthieuthuetdev <mthuet.pro@gmail.com>
2024-07-01 08:02:00 +0200

    push

--- date_naissance/public/js/script.js
+++ date_naissance/public/js/script.js
@@ -3,11 +3,17 @@ const app = {
     data() {
         return {
             date_naissance: "",
-            date_actuel: date.toLocaleDateString("fr-fr"),
-            age: 0
-        },
+            date_actuel: date,
+            age: 0,
+            phrase_age: ""
+        }
+    },
+    methods :{
         calculer_age(){
-            
+            const naissance = new Date(this.date_naissance)
+            this.age = this.date_actuel - this.date_naissance
+            console.log(this.age)
+
         }
     }
 }
