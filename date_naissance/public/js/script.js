e1058f08d1b1564b3c7015f7883a3f8b95548d2d
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-07-04 15:27:05 +0200

    ajout des phrase au fichier html et correction de quelque buges

--- date_naissance/public/js/script.js
+++ date_naissance/public/js/script.js
@@ -1,20 +1,21 @@
-const date = new Date()
+const current_date = new Date()
 const app = {
     data() {
         return {
-            date_naissance: "",
-            date_actuel: date,
-            age: 0,
-            phrase_age: ""
+            date_naissance: undefined,
+            date_naissance_formater: undefined,
+            date_actuel: current_date,
+            age: undefined
         }
     },
     methods: {
         calculer_age() {
             const naissance = new Date(this.date_naissance)
             const age = this.date_actuel - naissance
-            age = Math.floor(age / 1000 / 60 / 60 / 24 / 365)
-            this.age = age
-            console.log(this.age)
+            this.age = Math.floor(age / 1000 / 60 / 60 / 24 / 365)
+            const date_naissance_formater = new Date(naissance).toLocaleDateString()
+            this.date_naissance_formater = date_naissance_formater
+            console.log(this.date_naissance_formater)
 
         }
     }
