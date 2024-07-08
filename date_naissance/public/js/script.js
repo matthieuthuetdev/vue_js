4754f919d22e9a96ce0ea08e66df0830042601b9
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-07-08 11:52:00 +0200

    ajout d'une condition d'ajout d'un zéro a l'heure si besoins

--- date_naissance/public/js/script.js
+++ date_naissance/public/js/script.js
@@ -18,9 +18,10 @@ const app = {
             this.age = Math.floor(age / 1000 / 60 / 60 / 24 / 365)
             const date_naissance_formater = new Date(this.date_naissance).toLocaleDateString()
             this.date_naissance_formater = date_naissance_formater
-            this.heure_naissance = naissance.getHours() + ":"+ naissance.getMinutes()
-            
-
+            const heure = naissance.getHours() <= 9 ? "0"+naissance.getHours(): naissance.getHours()
+            const minute = naissance.getMinutes()<= 9 ? "0"+naissance.getMinutes(): naissance.getMinutes()
+            const seconde = naissance.getSeconds()<= 9 ? "0"+naissance.getSeconds(): naissance.getSeconds()
+            this.heure_naissance = heure + ":" + minute + ":" + seconde
         }
     }
 }
