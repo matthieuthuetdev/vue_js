6cdd8f028311e6df017ae0b2556d37cd96b71b42
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-07-08 11:26:35 +0200

    ajout de la date et de l'heure formater

--- date_naissance/public/js/script.js
+++ date_naissance/public/js/script.js
@@ -3,6 +3,9 @@ const app = {
     data() {
         return {
             date_naissance: undefined,
+            heure_naissance: undefined,
+            minute_naissance: undefined,
+            secondes_naissance: undefined,
             date_naissance_formater: undefined,
             date_actuel: current_date,
             age: undefined
@@ -13,9 +16,10 @@ const app = {
             const naissance = new Date(this.date_naissance)
             const age = this.date_actuel - naissance
             this.age = Math.floor(age / 1000 / 60 / 60 / 24 / 365)
-            const date_naissance_formater = new Date(naissance).toLocaleDateString()
+            const date_naissance_formater = new Date(this.date_naissance).toLocaleDateString()
             this.date_naissance_formater = date_naissance_formater
-            console.log(this.date_naissance_formater)
+            this.heure_naissance = naissance.getHours() + ":"+ naissance.getMinutes()
+            
 
         }
     }
