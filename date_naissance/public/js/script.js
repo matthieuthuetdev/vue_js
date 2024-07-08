1fa9def9af9d8ac00791f94002592a7aed6af1ba
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-07-08 11:55:59 +0200

    ajout des point virgule

--- date_naissance/public/js/script.js
+++ date_naissance/public/js/script.js
@@ -13,15 +13,15 @@ const app = {
     },
     methods: {
         calculer_age() {
-            const naissance = new Date(this.date_naissance)
-            const age = this.date_actuel - naissance
-            this.age = Math.floor(age / 1000 / 60 / 60 / 24 / 365)
-            const date_naissance_formater = new Date(this.date_naissance).toLocaleDateString()
-            this.date_naissance_formater = date_naissance_formater
-            const heure = naissance.getHours() <= 9 ? "0"+naissance.getHours(): naissance.getHours()
-            const minute = naissance.getMinutes()<= 9 ? "0"+naissance.getMinutes(): naissance.getMinutes()
-            const seconde = naissance.getSeconds()<= 9 ? "0"+naissance.getSeconds(): naissance.getSeconds()
-            this.heure_naissance = heure + ":" + minute + ":" + seconde
+            const naissance = new Date(this.date_naissance);
+            const age = this.date_actuel - naissance;
+            this.age = Math.floor(age / 1000 / 60 / 60 / 24 / 365);
+            const date_naissance_formater = new Date(this.date_naissance).toLocaleDateString();
+            this.date_naissance_formater = date_naissance_formater;
+            const heure = naissance.getHours() <= 9 ? "0"+naissance.getHours(): naissance.getHours();
+            const minute = naissance.getMinutes()<= 9 ? "0"+naissance.getMinutes(): naissance.getMinutes();
+            const seconde = naissance.getSeconds()<= 9 ? "0"+naissance.getSeconds(): naissance.getSeconds();
+            this.heure_naissance = heure + ":" + minute + ":" + seconde;
         }
     }
 }
